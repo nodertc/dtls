84ed93243df0ba335481a8aafdb9171730d58390
Dmitry Tsvettsikh <reklatsmasters@users.noreply.github.com>
2019-08-03 02:35:01 +0500

    server: handle cookie and respond by HelloVerifyRequest.

--- example.js
+++ example.js
@@ -1,7 +1,10 @@
 'use strict';
 
-Error.stackTraceLimit = Infinity;
+const dgram = require('dgram');
 const dtls = require('.');
+
+const serverSocket = dgram.createSocket('udp4');
+serverSocket.bind(4444);
 
 const socket = dtls.connect({
   type: 'udp4',
@@ -11,11 +14,11 @@ const socket = dtls.connect({
 });
 
 socket.on('error', err => {
-  console.error(err);
+  console.error('client:', err);
 });
 
 socket.on('data', data => {
-  console.log('got message "%s"', data.toString('ascii'));
+  console.log('client: got message "%s"', data.toString('ascii'));
   socket.close();
 });
 
@@ -24,7 +27,22 @@ socket.once('connect', () => {
 });
 
 socket.once('timeout', () => {
-  console.log('got timeout');
+  console.log('client: got timeout');
 });
 
 socket.setTimeout(5e3);
+
+const server = dtls.createServer({ socket: serverSocket });
+
+server.on('error', err => {
+  console.error('server:', err);
+});
+
+server.on('data', data => {
+  console.log('server: got message "%s"', data.toString('ascii'));
+  server.write(data, () => server.close());
+});
+
+socket.once('connect', () => {
+  console.log('server: got connecton');
+});
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@
 
 const { connect } = require('lib/socket');
 const { cipherSuites } = require('lib/constants');
+const CookieManager = require('lib/cookie-manager');
+const { createServer } = require('lib/server');
 
 module.exports = {
   connect,
+  createServer,
   constants: {
     cipherSuites: Object.assign({}, cipherSuites),
   },
+  CookieManager,
 };
